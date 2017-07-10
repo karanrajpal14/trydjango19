11ad52c2beb16a95e5d2b411999bd554803d6717
Karan Rajpal <rajpal.karan73@gmail.com>
2017-07-10 13:15:52 +0530

    Implemented static file handling and bootstrap

--- src/trydjango19/settings.py
+++ src/trydjango19/settings.py
@@ -118,3 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    # '/var/www/static/',
+]
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_cdn')
+# Files that the user uploads will come here
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media_cdn')
--- src/trydjango19/urls.py
+++ src/trydjango19/urls.py
@@ -18,6 +18,8 @@ Including another URLconf
 
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 # from posts import views as posts_views
 
 urlpatterns = [
@@ -30,3 +32,6 @@ urlpatterns = [
 
     url(r'^posts/', include("posts.urls", namespace='posts')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
