ecf451e8bf4f95eba3b6ac50ecbcfce041c1f46b
Karan Rajpal <rajpal.karan73@gmail.com>
2017-07-12 15:24:44 +0530

    Associating users to foregin keys to display post authors

--- src/posts/models.py
+++ src/posts/models.py
@@ -2,6 +2,7 @@ from django.core.urlresolvers import reverse
 from django.db import models
 from django.db.models.signals import pre_save
 from django.utils.text import slugify
+from django.conf import settings
 
 # Create your models here.
 
@@ -11,7 +12,7 @@ def upload_location(instance, filename):
 	return "%s/%s" %(instance.id, filename)
 
 class Post(models.Model):
-	
+	user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
 	title = models.CharField(max_length=120)
 	slug = models.SlugField(unique=True)
 
--- src/posts/views.py
+++ src/posts/views.py
@@ -8,9 +8,16 @@ from django.shortcuts import render, get_object_or_404, redirect
 def post_create(request):
 	if not request.user.is_staff or not request.user.is_superuser:
 		raise Http404
+
+	# To check if user is authenticated
+	# if not request.user.is_authenticated():
+	# 	raise Http404
+
 	form = PostForm(request.POST or None, request.FILES or None)
 	if form.is_valid():
 		instance = form.save(commit=False)
+		# Throws anonymous user error when not authenticated
+		instance.user = request.user
 		print(form.cleaned_data.get("title"))
 		print(form.cleaned_data.get("content"))
 		instance.save()
